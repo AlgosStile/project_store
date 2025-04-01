d26fa6cab972c464e6bb975c83a60879021f6dcb
AlgosStile <oleg.todor@mail.ru>
2025-04-01 16:52:12 +0300

    config commit

---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Извините, не удалось найти config.properties");
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
